071eace9afa00924de4d9e3360d9ff31128475d7
Baruch Phillips <baruchphillips@gmail.com>
2019-12-13 13:33:14 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,20 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new 
+  
+  end 
+  
+  # def edit 
+
+  def create 
+    # byebug
+    # THIS WORKED!!!!!!!
+    # Had issue for a really long time with permitting params - and then was writing 
+    # students plural instead of singular
+    student_params = params.require(:student).permit(:first_name, :last_name)
+    @student = Student.create(student_params) 
+    render :show
+  end 
+
 end
